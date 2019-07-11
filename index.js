c57181190bc3bbeb61b872009df590d87d03c705
Galvao Diniz Jonathan <jo.diniz@live.fr>
2019-07-11 10:58:32 +0200

    refacto code

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 const assert = require("assert");
 
-const arrayOfNumbers = [-10, 8, -20, 3, -500, -1, 2, 11, -100];
+const arrayOfNumbers = [-10, 8, -20, 3, -500, -100, 2, 11, -100, -2000];
 
 function dichotomie(array) {
   let arrayFirstPart = array.slice(0, array.length / 2);
-  let arraySecondPart = array.slice(array.length / 2, array.length);
+  let arrayLastPart = array.slice(array.length / 2, array.length);
 
   if (arrayFirstPart.length > 1) {
     arrayFirstPart = dichotomie(arrayFirstPart);
   }
-  if (arraySecondPart.length > 1) {
-    arraySecondPart = dichotomie(arraySecondPart);
+  if (arrayLastPart.length > 1) {
+    arrayLastPart = dichotomie(arrayLastPart);
   }
-  if (arrayFirstPart.length === 1 && arraySecondPart.length === 1) {
-    if (arrayFirstPart[0] > arraySecondPart[0]) {
-      return arraySecondPart;
-    }
-    return arrayFirstPart;
-  }
+
+  return arrayFirstPart[0] > arrayLastPart[0] ? arrayLastPart : arrayFirstPart;
 }
 
 console.log("TCL: dichotomie -> dichotomie", dichotomie(arrayOfNumbers));
@@ -25,9 +21,9 @@ console.log("TCL: dichotomie -> dichotomie", dichotomie(arrayOfNumbers));
 // tests
 
 assert.deepEqual(
-  dichotomie([-10, 8, -20, 3, -500, -1, 2, 11, -100]),
-  [-500],
-  "dichotomie([-10, 8, -20, 3, -500, -1, 2, 11, -100]) do not return [-500], function is broken !!"
+  dichotomie([-10, 8, -20, 3, -500, -1, 2, 11, -10000000000]),
+  [-10000000000],
+  "dichotomie([-10, 8, -20, 3, -500, -1, 2, 11, -10000000000]) do not return [-10000000000], function is broken !!"
 );
 
 assert.deepEqual(
